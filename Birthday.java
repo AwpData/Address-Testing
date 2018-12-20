eb0b6806fc98e55f4986957636d9a078e5dca8ca
AwpData <awpdata@gmail.com>
2018-12-19 17:00:42 -0800

    Updated Birthday to use User Input

--- Birthday.java
+++ Birthday.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 public class Birthday {
-    private int month;
-    private int day;
-    private int year;
-    
-    public Birthday(int month, int day, int year) {
-        this.month = month;
-        this.day = day;
-        this.year = year;
-    }
-    
-    public String toString() {
-        return month + "/" + day + "/" + year;
-    }
+	private String month;
+	private String day;
+	private String year;
+
+	public Birthday() {
+		System.out.println();
+		System.out.println("Now logging birthday");
+		this.month = setMonth();
+		this.day = setDay();
+		this.year = setYear();
+	}
+
+	public Birthday(String month, String day, String year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+	}
+
+	public String setMonth() {
+		System.out.println("Enter Month");
+		month = getInput();
+		return month;
+	}
+
+	public String setDay() {
+		System.out.println("Enter Day");
+		day = getInput();
+		return day;
+	}
+
+	public String setYear() {
+		System.out.println("Enter Year");
+		year = getInput();
+		return year;
+	}
+
+	public String toString() {
+		return month + "/" + day + "/" + year;
+	}
+
+	public String getInput() {
+		Scanner input = new Scanner(System.in);
+		String date = input.nextLine();
+		return date;
+	}
 }
+
