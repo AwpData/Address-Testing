225b109291f4bc0898060fa21db5f816d1976aa8
AwpData <awpdata@gmail.com>
2018-12-19 17:01:16 -0800

    Added user input

--- Client.java
+++ Client.java
@@ -1,11 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Address {
-    public static void main(String[] args) {
-        System.out.println("This program prints your address and now your birthday!");
-        Birthday birthday = new Birthday(11, 13, 1998);
-        newAddress AddressFull = new newAddress("Trevor", "Beach St", "Los Angeles", "California", birthday);
-        newAddress AddressFull2 = new newAddress("Who", "What St", "Where City", "Why State", birthday);
-        newAddress AddressFull3 = new newAddress("Yo", "Hello St", "Hola City", "Bonjour State", birthday);
-        System.out.println("Total Records: " + newAddress.getRecords());
- 
-    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String choice = "HI";
+		System.out.println("This program prints multiple addresses, along with their birthdays for reference");
+		List<newAddress> Addresses = new ArrayList<newAddress>();
+		while (!choice.equals("3")) {
+			System.out.println("What do you want to do?");
+			System.out.println("1. Add an address");
+			System.out.println("2. Print out all address entries");
+			System.out.println("3. Quit");
+			choice = input.nextLine();
+			if (choice.equals("1")) {
+				newAddress Address = new newAddress();
+				Addresses.add(Address);
+				System.out.println("Address added to records...");
+				System.out.println();
+			}
+			if (choice.equals("2")) {
+				for (int i = 0; i < Addresses.size(); i++) {
+					Addresses.get(i).printFullAddress();
+				}
+				System.out.println("Total Entries: " + newAddress.getEntries());
+				System.out.println();
+			}
+		}
+		System.out.println("Quitting");
+	}
 }
+
